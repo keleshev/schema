d51676ae72009a48776e24e6a8b7e00151b9fca1
Simon Jakobi <simon.jakobi@gmail.com>
2015-09-26 22:02:09 +0200

    Add Python3.4 and 3.5 to setup.py

--- setup.py
+++ setup.py
@@ -22,6 +22,8 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: Implementation :: PyPy",
         "License :: OSI Approved :: MIT License",
     ],
