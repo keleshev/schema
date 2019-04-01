4a0bf6f509e6b69956a8f2fd4e1c3873fc419be8
Oliver Bristow <evilumbrella+github@gmail.com>
2019-04-01 18:17:17 +0100

    Set long_description_content_type (#202)
    This is so things like [pypi.org](https://pypi.org/project/schema/) don't have to guess how to render the package description.

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
     url="https://github.com/keleshev/schema",
     py_modules=["schema"],
     long_description=codecs.open("README.rst", "r", "utf-8").read(),
+    long_description_content_type='text/x-rst',
     install_requires=open("requirements.txt", "r").read().split("\n"),
     classifiers=[
         "Development Status :: 3 - Alpha",
