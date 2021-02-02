e63ee9e351b6cb2ac85e835da5a7b4d64022f148
Christian Clauss <cclauss@me.com>
2021-02-02 11:54:47 +0100

    Travis CI: Add Python 3.9 to the testing (#251)
    * Travis CI: Add Python 3.9 to the testing
    
    * tox.ini: Add py39
    
    * setup.py: Programming Language :: Python :: 3.9

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: PyPy",
         "License :: OSI Approved :: MIT License",
     ],
